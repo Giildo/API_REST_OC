b2c4a6010488d53a4d545a986b80d9c4e5bceb4f
ptraon <ptraon@gmail.com>
2018-11-04 11:17:54 +0100

    méthode POST donne une erreur 500

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -26,4 +26,20 @@ class ArticleController extends Controller
 
         return $response;
     }
+
+    /**
+     * @Route("/articles", name="article_create")
+     * @Method({"POST"})
+     */
+    public function createAction(Request $request)
+    {
+        $data = $request->getContent();
+        $article = $this->get('jms_serializer')->deserialize($data, 'AppBundle\Entity\Article', 'json');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($article);
+        $em->flush();
+
+        return new Response('', Response::HTTP_CREATED);
+    }
 }
